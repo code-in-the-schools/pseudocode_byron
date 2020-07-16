3e630c06e19386ee16bd25967c266c5290f09892
byronbrown <68286443+byronbrown@users.noreply.github.com>
2020-07-16 15:42:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Take User input
+#convert input to str
+Userinput =str(input("Give me a character to turn into a diamond..."))
+
+#diamond variable 1
+s_1= userinput+"  "+userinput"  "+userinput"  "+userinput"  "
+#diamond variable 2 
+s_2=userinput+"    "+Userinput
+
+
+print(s_1)
+print(s_2)
